a068be51d13098e66f311646323e509a5c5acc38
siddhant <sidsingh250496@gmail.com>
2023-08-09 15:05:03 +0530

    Validation Error check added

--- src/main/java/com/valmiki/adhyapakservice/dto/request/CourseRequest.java
+++ src/main/java/com/valmiki/adhyapakservice/dto/request/CourseRequest.java
@@ -1,14 +1,20 @@
 package com.valmiki.adhyapakservice.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
+
 @Builder
 @Data
+@JsonIgnoreProperties (ignoreUnknown = true)
 public class CourseRequest {
 
     private int id;
-
+    @JsonProperty("courseName")
+    @NotBlank
     private String courseName;
 
     private String subHeading;
